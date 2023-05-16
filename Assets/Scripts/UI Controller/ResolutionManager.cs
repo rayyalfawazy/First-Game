01babe7c3d6358051f6b548a9d09fc092748c295
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-05-16 16:52:35 +0700

    Add: Volume and resolution setting view

---
+++ Assets/Scripts/UI Controller/ResolutionManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+    [SerializeField] TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    List<Resolution> filteredResolution;
+
+    float currentRefreshRate; 
+    int currentResolutionIndex = 0;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions; // Ambil data Resolution dari Screen System Resolution
+        filteredResolution = new List<Resolution>();
+
+        resolutionDropdown.ClearOptions();
+        currentRefreshRate = Screen.currentResolution.refreshRate; // Ambil data Refresh Rate dari Screen System Resolution
+
+        for (int i = 0; i < resolutions.Length; i++) 
+        {
+            if (resolutions[i].refreshRate == currentRefreshRate) // Ambil kondisi dimana Refresh Rate System sama dengan Refre
+            {
+                filteredResolution.Add(resolutions[i]);
+            }
+        }
+
+        List<string> options = new List<string>();
+        for (int i = 0;i < filteredResolution.Count; i++) 
+        {
+            string resolutionOption = $"{filteredResolution[i].width} x {filteredResolution[i].height}";
+            options.Add(resolutionOption);
+            if (filteredResolution[i].width == Screen.width && filteredResolution[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredResolution[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+}
